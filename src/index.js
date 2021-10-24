609a4522becc0e2e852eaa56a16d9cdb7a5a6db6
LindsayMuji <91750459+LindsayMuji@users.noreply.github.com>
2021-10-24 00:09:44 -0500

    modificacion de html y css, ingreso de codigo en JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
 import validator from './validator.js';
 
+const formulario = document.querySelector('#formulario-tarjeta');
+
+
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
+}
+const yearActual = new Date().getFullYear;
+for (let i = yearActual; i <= yearActual + 8; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
+//* input de numero de tarjeta
+formulario.inputnumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+    formulario.inputNumero.value = valorInput
+//* elimino espacios en blanco 
+    .replace(/\s/g, '')
+//* elimino las letras
+    .replace(/\D/g, '')
+//* espaciado cada cuatro digitos
+.replace(/([0-9]{4})/g), '$1 ')
+//* elimina el ultimo espacio
+.trim();
+})
+
+if(valorInput==''){
+    #inputNumero = 'Ingrese Número de Tarjeta'
+}
+//* input nombre
+formulario.inputNombre.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+//* eliminar numeros en el nombre
+    formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+if (valorInput == ''){
+    inputNombre = 'Ingrese Nombre'
+}
+})
+
+//* Select del mes
+formulario.selectMes.addEventListener('change', (e)=>{
+    
+})
+
+
+
+
+
 console.log(validator);
 var = numeroTarjeta
-
+function cardnumber(inputtxt) 
+{
+    
+}
     
 }
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,15 @@
-const validator = {
-  // ...
-};
+const validator (n) = {
+  // Convertir string en array
+const digits = n.toString().split('').map(number);
 
+const sum = digits
+// Duplicar cada segundo digito de derecha a izquierda
+.map((digit,idx) => idx % 2 === digits.length % 2 ? fixDouble(digit * 2):digit
+//suma de digitos mayores a 9
+.reduce((acc, digit) => acc += digit, 0)
+return sum % 10 === 0;
+};
+function fixDouble(number) {
+  return number > 9 ? number - 9 : number;
+  }
 export default validator;
