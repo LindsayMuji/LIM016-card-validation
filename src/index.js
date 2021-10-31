cd87dad688959a6706f65a24796b7ff7407cca4c
LindsayMuji <lmelissa.mp@gmail.com>
2021-10-30 21:00:14 -0500

    ingreso de datos al validator y algoritmo de Lunh

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import validator from './validator.js';
-    console.log(validator.isValid("inputNumero"));
+
 
 const formulario = document.querySelector('#formulario-tarjeta');
 let inputNumero = document.querySelector('#inputNumero');
-let inputNombre = document.querySelector('#inputNombre');
-var btn = document.getElementById('btn-validar');
+let inputName = document.querySelector('#inputNombre');
+let botonValidar = document.getElementById('btn-validar');
 
 
-//* ingresar datos del mes 
+//* ingresar datos del mes
     for (let i = 1; i <= 12; i++) {
         let opcion = document.createElement('option');
         opcion.value = i;
         opcion.innerText = i;
-        formulario.selectMes.appendChild(opcion);    
+        formulario.selectMes.appendChild(opcion);
     }
 
 //* ingresar datos del año
@@ -28,7 +28,7 @@ var btn = document.getElementById('btn-validar');
 formulario.inputNumero.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
     formulario.inputNumero.value = valorInput
-//* elimino espacios en blanco 
+//* elimino espacios en blanco
     .replace(/\s/g, '')
 //* elimino las letras
     .replace(/\D/g, '')
@@ -43,14 +43,21 @@ if(inputNumero==''){
 }
 
 //* input nombre
-formulario.inputNombre.addEventListener('keyup', (e) => {
+formulario.inputName.addEventListener('keyup', (e) => {
     let valorInput = e.target.value;
     //* eliminar numeros en el nombre
-    formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+    formulario.inputName.value = valorInput.replace(/[0-9]/g, '');
     if (valorInput == ''){
-        inputNombre = 'Ingrese Nombre'
-    }   
+        alert ('Ingrese Nombre')
+    }
 })
 
+botonValidar.addEventListener('click', () => {
+  let digitos = document.getElementById('inputNumero').value;
+  let resultado = validator.isValid(digitos.replace(/\s/g, ''));
+  let masknumber = validator.maskify(digitos.replace(/\s/g, ''))
 
+  resultado === true? alert("El número de tarjeta: " + "\n" + masknumber + "\n \n" + "ES VALIDA" ):
+  alert("El número de tarjeta: " + "\n" + masknumber + "\n \n" + "NO ES VALIDA");
+});
 
--- src/validator.js
+++ src/validator.js
@@ -1,28 +1,45 @@
 const validator = {
   isValid: function (inputNumero) {
-    if (/[^0-9-\s]+/.test(value)) return false;
-    // The Luhn Algorithm. It's so pretty.
-        var nCheck = 0, nDigit = 0, bEven = false;
-        value = value.replace(/\D/g, "");
-    
-        for (var n = value.length - 1; n >= 0; n--) {
-            var cDigit = value.charAt(n),
-                nDigit = parseInt(cDigit, 10);
-    
-            if (bEven) {
-                if ((nDigit *= 2) > 9) nDigit -= 9;
-            }
-    
-            nCheck += nDigit;
-            bEven = !bEven;
+    //Convertir numero en Array´
+    let digitos = Array.from(inputNumero).map(Number);
+    //algoritmo de Lunh
+
+    //Invertir numeros
+    digitos = digitos.reverse();
+
+    let sum = 0;
+
+    for (let i = 0; i < digitos.lenght; i++) {
+      if (i%2===1){
+        if ((digitos[i]*2) >=10){
+          digitos[i] = digitos[i]*2-9;
+        } else{
+          digitos[i] = digitos[i]*2;
         }
-    
-        return (nCheck % 10) == 0;
+      } else{
+        digitos[i];
+      }
+      //suma de digitos
+      sum = sum + digitos[i];
     }
-  //  return inputNumero},
- // maskify: function () {return "nombre"}
-//};
+    //si la suma de los digitos es multiplo de 10 es tarjeta valida
+    return sum%10 ===0;
+  },
 
+  maskify: (inputNumero) => {
 
-export default: validator,
+    let mask = " ";
+
+    for (let i = 0; i < inputNumero.lenght; i++){
+
+      if (i<= inputNumero.lenght -5){
+        mask += "#";
+      } else{
+        mask += inputNumero[i];
+      }
+    }
+
+    return mask;
   }
+};
+  export default validator;
